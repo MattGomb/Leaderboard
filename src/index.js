633f8773189e14082348efe443c7a683b623df47
Matyas Gombos <gombosmm@gmail.com>
2022-10-18 17:59:25 -0500

    Add functions of localstorage and display

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
+import Score from './modules/constructor';
+
+import Display from './modules/display';
+
+import Storage from './modules/storage';
+
 import './style.css'
 
+const container = document.querySelector('#score-list-container')
+container.innerHTML = '';
+
+container.appendChild(Display.displayScores(Storage.getScores()));
+
+document.querySelector('#form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const name = document.querySelector('#name-input').value;
+  const scorevalue = document.querySelector('#score-input').value
+
+  let scores = [];
+  if (localStorage.getItem('scores') !== null) {
+    scores = JSON.parse(localStorage.getItem('scores'));
+  }
+  const score = new Score(name, scorevalue);
+
+  const list = document.querySelector('#score-list'); 
+
+  list.appendChild(Display.addScoreToBoard(score));
+
+  Storage.addScore(score);
+
+  e.target.reset();
+});
---
+++ src/modules/display.js
@@ -0,0 +1,29 @@
+export default class Display {
+  static displayScores(scores) {
+    const list = document.createElement('ul');
+    list.id = 'score-list';
+    if (scores.length !== 0) {
+      scores.forEach((score) => {
+        list.appendChild(Display.addScoreToBoard(score));
+      });
+    }
+    return list;
+  }
+
+  static addScoreToBoard( { name, scorevalue } ) {
+    const newEntry = document.createElement('li');
+    newEntry.setAttribute('class', 'entry');
+
+    const user = document.createElement('p');
+    user.classList.add('name');
+    user.textContent = name.length > 30 ? 'Someone with a long name ;)' : name;
+    newEntry.appendChild(user);
+
+    const scorevalue1 = document.createElement('p');
+    scorevalue1.classList.add('score');
+    scorevalue1.textContent = scorevalue.length > 10 ? 'A really big score ;)' : scorevalue;
+    newEntry.appendChild(scorevalue1);
+
+    return newEntry;
+  }
+}
